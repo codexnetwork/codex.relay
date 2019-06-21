47ece5abe7c06ee54c25110688f9978b891f73d6
xuyp1991 <xuyapeng1991@qq.com>
2019-06-21 18:08:01 +0800

    get relay currency balance commit

--- plugins/chain_api_plugin/chain_api_plugin.cpp
+++ plugins/chain_api_plugin/chain_api_plugin.cpp
@@ -95,6 +95,7 @@ void chain_api_plugin::plugin_startup() {
       CHAIN_RO_CALL(get_table_rows, 200),
       CHAIN_RO_CALL(get_table_by_scope, 200),
       CHAIN_RO_CALL(get_currency_balance, 200),
+      CHAIN_RO_CALL(get_relay_currency_balance, 200),
       CHAIN_RO_CALL(get_currency_stats, 200),
       CHAIN_RO_CALL(get_producers, 200),
       CHAIN_RO_CALL(get_producer_schedule, 200),
